babe8115aeb9eb0e5d83ffbdf6b1a71f58ec3aa2
Hudson-Pufferfish <hungson.nguyen811@gmail.com>
2022-10-15 22:26:48 -0500

    use redux persist and config persist for root

--- redux/store.js
+++ redux/store.js
@@ -1,8 +1,26 @@
-// reducers func
 import { configureStore } from "@reduxjs/toolkit";
+import { combineReducers } from 'redux';
 import cartReducer from "./slices/cartSlice";
+// AsyncStorage replace of local storage
+import AsyncStorage from '@react-native-async-storage/async-storage';
+import { persistReducer, persistStore } from 'redux-persist';
 
-export const store = configureStore({
-  reducer: cartReducer,
-  devTools: process.env.NODE_ENV !== 'production',
+
+const rootReducer = combineReducers({
+  cart: cartReducer,
+  // product: productReducer
 })
+const persistConfig = {
+  key: 'root',
+  storage: AsyncStorage,
+};
+
+const persistedReducer = persistReducer(persistConfig, rootReducer);
+
+export default () => {
+  let store = configureStore({
+    reducer: persistedReducer,
+  })
+  let persistor = persistStore(store)
+  return { store, persistor }
+}
