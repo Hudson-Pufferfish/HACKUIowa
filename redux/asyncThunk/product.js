0dddb21f98d89130eb534a42abe87100ff4dd6c2
Minh Huyen Nguyen <huyennm2002@gmail.com>
2022-10-16 06:53:09 -0500

    redirect and pass params to item screen

--- redux/asyncThunk/product.js
+++ redux/asyncThunk/product.js
@@ -1,6 +1,6 @@
 export default data = [
     {
-        "barcode": "028400090148",
+        "barcode": "0028400090148",
         "carbon": 150,
         "category": "Food, Beverages & Tobacco > Food Items > Snack Foods > Chips",
         "id": "8b9fcda6-b953-4f1e-bb98-cd61679c26e3",
@@ -8,7 +8,7 @@ export default data = [
         "name": "Ruffles® Original Potato Chips 1 Oz. Bag"
     },
     {
-        "barcode": "012000018770",
+        "barcode": "0012000018770",
         "carbon": 205,
         "category": "Food, Beverages & Tobacco > Beverages > Soda",
         "id": "43caf0e5-11ad-4dc8-b2f3-11448114048a",
@@ -16,7 +16,7 @@ export default data = [
         "name": "Pepsi Zero Sugar Cola Soda, 12 Fl. Oz."
     },
     {
-        "barcode": "038000138577",
+        "barcode": "0038000138577",
         "carbon": 150,
         "category": "Food, Beverages & Tobacco > Food Items > Snack Foods > Chips",
         "id": "f141b169-c11b-4563-8ed9-0a3322967247",
@@ -24,7 +24,7 @@ export default data = [
         "name": "Pringles 9133562 5.57 Oz Cheddar Cheese Flavored Potato Chips Can- Pack of 14 (299611)"
     },
     {
-        "barcode": "041716231104",
+        "barcode": "0041716231104",
         "carbon": 205,
         "category": "Food, Beverages & Tobacco > Food Items > Dairy Products > Cheese",
         "id": "a43c14b0-86d8-44b0-ada4-751a65093c9e",
--- redux/asyncThunk/productAsyncThunk.js
+++ redux/asyncThunk/productAsyncThunk.js
@@ -1,48 +1,62 @@
 import { createAsyncThunk } from '@reduxjs/toolkit'
 import axios from 'axios'
 import uuid from 'react-native-uuid';
+import data from './product.js'
+// export const fetchProductInfo = createAsyncThunk(
+//     'product/fetchByBarCode',
+//     async (productBarCode, thunkAPIs) => {
+//         await axios({
+//             method: 'GET',
+//             url: 'https://barcode-lookup.p.rapidapi.com/v3/products',
+//             params: {barcode: productBarCode},
+//             headers: {
+//                 'X-RapidAPI-Key': '48ffbb5cb5mshe7d78491933dec9p1b0bc7jsn85439d123583',
+//                 'X-RapidAPI-Host': 'barcode-lookup.p.rapidapi.com'
+//             }
+//         }).then((res) => {
+//             const data = res.data.products[0];
+//             return {
+//                 id: uuid.v4(),
+//                 image: data.images,
+//                 barcode: data.barcode_number,
+//                 name: data.title,
+//                 category: data.category,
+//                 carbon: carbonCalculator(data.category), 
+//             };
+//         });
+//     }
+// )
+
+// const carbonCalculator = (category) => {
+//     const data = category.toLowerCase();
+//     switch(true) {
+//         case data.includes('snack'):
+//             return 150
+//         case data.includes('beverages'):
+//             return 205
+//         case data.includes('eggs'):
+//             return 2000
+//         case data.includes('meat'):
+//             return 20000
+//         case data.includes('vegetables'):
+//             return 1000
+//         case data.includes('oil'):
+//             return 1200
+//         default: 
+//             return 'NA'
+//     }
+// }
 
 export const fetchProductInfo = createAsyncThunk(
     'product/fetchByBarCode',
-    async (productBarCode, thunkAPIs) => {
-        await axios({
-            method: 'GET',
-            url: 'https://barcode-lookup.p.rapidapi.com/v3/products',
-            params: {barcode: productBarCode},
-            headers: {
-                'X-RapidAPI-Key': '48ffbb5cb5mshe7d78491933dec9p1b0bc7jsn85439d123583',
-                'X-RapidAPI-Host': 'barcode-lookup.p.rapidapi.com'
-            }
-        }).then((res) => {
-            const data = res.data.products[0];
-            return {
-                id: uuid.v4(),
-                image: data.images,
-                barcode: data.barcode_number,
-                name: data.title,
-                category: data.category,
-                carbon: carbonCalculator(data.category), 
-            };
-        });
+    (productBarCode, thunkAPIs) => {
+        try {
+            const res = data.filter(prod => {
+                return prod.barcode == productBarCode
+            });
+            return res;
+        } catch (err) {
+            return rejectWithValue(err);
+        }
     }
 )
-
-const carbonCalculator = (category) => {
-    const data = category.toLowerCase();
-    switch(true) {
-        case data.includes('snack'):
-            return 150
-        case data.includes('beverages'):
-            return 205
-        case data.includes('eggs'):
-            return 2000
-        case data.includes('meat'):
-            return 20000
-        case data.includes('vegetables'):
-            return 1000
-        case data.includes('oil'):
-            return 1200
-        default: 
-            return 'NA'
-    }
-}
