66c93fe241cf62497b7a10be0b4b2d79b506ca28
Minh Huyen Nguyen <huyennm2002@gmail.com>
2022-10-15 23:51:40 -0500

    add product reducer

--- redux/store.js
+++ redux/store.js
@@ -1,6 +1,7 @@
 import { configureStore } from "@reduxjs/toolkit";
 import { combineReducers } from 'redux';
 import cartReducer from "./slices/cartSlice";
+import productReducer from "./slices/productSlice"
 // AsyncStorage replace of local storage
 import AsyncStorage from '@react-native-async-storage/async-storage';
 import { persistReducer, persistStore } from 'redux-persist';
@@ -8,7 +9,7 @@ import { persistReducer, persistStore } from 'redux-persist';
 
 const rootReducer = combineReducers({
   cart: cartReducer,
-  // product: productReducer
+  product: productReducer
 })
 const persistConfig = {
   key: 'root',
