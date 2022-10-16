32691f6f28f85f2252a5f278e9ca654353b645a9
Hudson-Pufferfish <hungson.nguyen811@gmail.com>
2022-10-16 03:55:30 -0500

    styled the button of 3 screens

--- screens/HomeScreen.jsx
+++ screens/HomeScreen.jsx
@@ -1,42 +1,21 @@
-import { Button, View, Text, TouchableOpacity, StyleSheet } from 'react-native'
+import { Button, View, Text, TouchableOpacity } from 'react-native'
 import React from 'react'
+import { buttonStyles } from '../styles/button.styles';
 
 TouchableOpacity.defaultProps = { activeOpacity: 0.8 };
 
 const AppButton = ({ onPress, title }) => (
-  <TouchableOpacity onPress={onPress} style={styles.appButtonContainer}>
-    <Text style={styles.appButtonText}>{title}</Text>
+  <TouchableOpacity onPress={onPress} style={buttonStyles.appButtonContainer}>
+    <Text style={buttonStyles.appButtonText}>{title}</Text>
   </TouchableOpacity>
 );
 
 const HomeScreen = ({ navigation }) => {
  return (
-    <View style= {styles.screenContainer}>
+    <View style= {buttonStyles.screenContainer}>
       <AppButton title="Scanning" size="sm" backgroundColor="#007bff" onPress={() => navigation.push('Camera')}/>
     </View>
   )
 }
-
-const styles = StyleSheet.create({
-  screenContainer: {
-    flex: 1,
-    justifyContent: "center",
-    padding: 16
-  },
-  appButtonContainer: {
-    elevation: 8,
-    backgroundColor: "#009688",
-    borderRadius: 10,
-    paddingVertical: 10,
-    paddingHorizontal: 12
-  },
-  appButtonText: {
-    fontSize: 18,
-    color: "#fff",
-    fontWeight: "bold",
-    alignSelf: "center",
-    textTransform: "uppercase"
-  }
-});
 
 export default HomeScreen
