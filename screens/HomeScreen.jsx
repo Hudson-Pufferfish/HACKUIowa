bb58cab8c0bdfc0221216f300712eacea5ea1dac
Huy Truong <truonggiahuy2410bd@gmail.com>
2022-10-16 02:48:04 -0500

    Customize Scanning Button on HomeScreen

--- screens/HomeScreen.jsx
+++ screens/HomeScreen.jsx
@@ -1,13 +1,42 @@
-import { Button, View, Text } from 'react-native'
+import { Button, View, Text, TouchableOpacity, StyleSheet } from 'react-native'
 import React from 'react'
 
+TouchableOpacity.defaultProps = { activeOpacity: 0.8 };
+
+const AppButton = ({ onPress, title }) => (
+  <TouchableOpacity onPress={onPress} style={styles.appButtonContainer}>
+    <Text style={styles.appButtonText}>{title}</Text>
+  </TouchableOpacity>
+);
 
 const HomeScreen = ({ navigation }) => {
  return (
-    <View style={{ flex: 1, alignItems: 'center', justifyContent: 'center' }}>
-      <Button title="Scanning" onPress={() => navigation.push('Camera')}/>
+    <View style= {styles.screenContainer}>
+      <AppButton title="Scanning" size="sm" backgroundColor="#007bff" onPress={() => navigation.push('Camera')}/>
     </View>
   )
 }
+
+const styles = StyleSheet.create({
+  screenContainer: {
+    flex: 1,
+    justifyContent: "center",
+    padding: 16
+  },
+  appButtonContainer: {
+    elevation: 8,
+    backgroundColor: "#009688",
+    borderRadius: 10,
+    paddingVertical: 10,
+    paddingHorizontal: 12
+  },
+  appButtonText: {
+    fontSize: 18,
+    color: "#fff",
+    fontWeight: "bold",
+    alignSelf: "center",
+    textTransform: "uppercase"
+  }
+});
 
 export default HomeScreen
