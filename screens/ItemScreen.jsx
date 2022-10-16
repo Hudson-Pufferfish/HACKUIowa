e612c9cf01d04e6c11a6639fb2080aa79acc5db2
Hudson-Pufferfish <hungson.nguyen811@gmail.com>
2022-10-16 14:15:00 -0500

    edit Item screen

--- screens/ItemScreen.jsx
+++ screens/ItemScreen.jsx
@@ -1,11 +1,25 @@
-import { Button, View, Text } from 'react-native'
-import React from 'react'
+import { ImageBackground, Button, View, Text, TouchableOpacity } from 'react-native';
+import React from 'react';
+import { buttonStyles } from '../styles/button.styles';
+
+TouchableOpacity.defaultProps = { activeOpacity: 0.8 };
+
+const AppButton = ({ onPress, title }) => (
+  <TouchableOpacity onPress={onPress} style={buttonStyles.appButtonContainer}>
+    <Text style={buttonStyles.appButtonText}>{title}</Text>
+  </TouchableOpacity>
+);
 
 const ItemScreen = ( {navigation} ) => {
   return (
-    <View style={{ flex: 1, alignItems: 'center', justifyContent: 'center' }}>
-      <Text>Item Screen</Text>
-      <Button title="Add to Cart" onPress={() => navigation.navigate('Cart')} />
+    // <View style={buttonStyles.screenContainer}>
+    //   <Button title="Add to Cart" onPress={() => navigation.navigate('Cart')} />
+    // </View>
+    
+    <View style={buttonStyles.screenContainer}>
+    <ImageBackground source={require("../assets/images/treeBg.jpg")} style={buttonStyles.image}>
+      <AppButton title="Add to Cart" size="sm" backgroundColor="#007bff" onPress={() => navigation.navigate('Cart')}/>
+    </ImageBackground>
   </View>
   )
 }
